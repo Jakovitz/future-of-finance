b554594318b8e350427dbf370d79bc8cff024f2d
b990595 <kimrasmus@gmail.com>
2016-03-14 09:13:42 +0100

    Update spar-nord2.js

--- src/main/webapp/spar-nord2.js
+++ src/main/webapp/spar-nord2.js
@@ -15,7 +15,7 @@ Spar Nord har i samarbejde med Kræftens Bekæmpelse og Sygehus Nord indgået et
 Og hvor Spar Nord fordobler alle donationer frem til 1. maj.\
 </p>\
 <p>\
-<img src="https://fof.local:8443/swipp.svg" style="height: 26px; width: auto; position: relative; top: 4px;" /> <a id="microinject-init" href="onclick();" style="text-decoration: none;">Doner beløb</a>\
+<img src="https://raw.githubusercontent.com/Jakovitz/future-of-finance/master/src/main/webapp/swipp.svg" style="height: 26px; width: auto; position: relative; top: 4px;" /> <a id="microinject-init" href="onclick();" style="text-decoration: none;">Doner beløb</a>\
 </p>';
 
 document.title = "Spar Nord fordobler donationer til ny taghave på Sygehus Nords børneafdeling for kræftramte børn";
@@ -25,22 +25,22 @@ window.history.pushState('page2', 'Title', '/om-spar-nord/presse/pressemeddelels
 
     $('div.head').html(articleHead);
     $('div.intro').find('div.pad').html(articleAndDonate);
-    $('div.intro').find('div.rgt').find('img').attr('src', 'https://fof.local:8443/image.jpg');
-    $('div.intro').find('div.rgt').find('img').attr('data-original', 'https://fof.local:8443/image.jpg');
-    $('div.intro').find('div.rgt').find('img').attr('src', 'https://fof.local:8443/image.jpg');
+    $('div.intro').find('div.rgt').find('img').attr('src', 'https://raw.githubusercontent.com/Jakovitz/future-of-finance/master/src/main/webapp/image.jpg');
+    $('div.intro').find('div.rgt').find('img').attr('data-original', 'https://raw.githubusercontent.com/Jakovitz/future-of-finance/master/src/main/webapp/image.jpg');
+    $('div.intro').find('div.rgt').find('img').attr('src', 'https://raw.githubusercontent.com/Jakovitz/future-of-finance/master/src/main/webapp/image.jpg');
 
 
 
     $('body').append('<div id="microinject-dialog" title="Betal med Swipp">\
         <form class="buy-form"><a href="#" style="display: block; margin-top: 10px; outline: 0; color: #d00; font-weight: 700; font-size: 13px;">Jeg er ikke Rasmus (20894692)</a></form>\
-        <a class="buy" style="margin-top: 17px;"><img src="https://fof.local:8443/swipp.svg" style="height: 22px;"><span style="float: right;">Kr. 5</span></a>\
-        <a class="buy"><img src="https://fof.local:8443/swipp.svg" style="height: 22px;"><span style="float: right;">Kr. 10</span></a>\
-        <a class="buy"><img src="https://fof.local:8443/swipp.svg" style="height: 22px;"><span style="float: right;">Kr. 20</span></a>\
-        <a id="microinject-buy" class="buy"><img src="https://fof.local:8443/swipp.svg" style="height: 22px;"><span style="float: right;">Kr. 50</span></a>\
+        <a class="buy" style="margin-top: 17px;"><img src="https://raw.githubusercontent.com/Jakovitz/future-of-finance/master/src/main/webapp/swipp.svg" style="height: 22px;"><span style="float: right;">Kr. 5</span></a>\
+        <a class="buy"><img src="https://raw.githubusercontent.com/Jakovitz/future-of-finance/master/src/main/webapp/swipp.svg" style="height: 22px;"><span style="float: right;">Kr. 10</span></a>\
+        <a class="buy"><img src="https://raw.githubusercontent.com/Jakovitz/future-of-finance/master/src/main/webapp/swipp.svg" style="height: 22px;"><span style="float: right;">Kr. 20</span></a>\
+        <a id="microinject-buy" class="buy"><img src="https://raw.githubusercontent.com/Jakovitz/future-of-finance/master/src/main/webapp/swipp.svg" style="height: 22px;"><span style="float: right;">Kr. 50</span></a>\
     </div>');
 
     $('body').append('<div id="microinject-receipt-dialog" title="Bekræft donationen">\
-        <a id="microinject-confirm" class="buy"><img src="https://fof.local:8443/swipp.svg" style="height: 22px;"><span style="float: right;">Kr. 50</span></a>\
+        <a id="microinject-confirm" class="buy"><img src="https://raw.githubusercontent.com/Jakovitz/future-of-finance/master/src/main/webapp/swipp.svg" style="height: 22px;"><span style="float: right;">Kr. 50</span></a>\
     </div>');
 
     $('.subscriber-hide a').each(function () {
@@ -91,12 +91,12 @@ window.history.pushState('page2', 'Title', '/om-spar-nord/presse/pressemeddelels
 
             $('#microinject-confirm').on('click', function (e) {
                 $('#microinject-confirm').addClass('busy');
-                $('#microinject-confirm').html('<img src="https://fof.local:8443/swipp.svg" style="height: 22px;"><span style="float: right;">Behandler</span>');
+                $('#microinject-confirm').html('<img src="https://raw.githubusercontent.com/Jakovitz/future-of-finance/master/src/main/webapp/swipp.svg" style="height: 22px;"><span style="float: right;">Behandler</span>');
 
                 setTimeout(function () {
                     $('#microinject-confirm').addClass('thanks');
                     $('#ui-dialog-title-microinject-receipt-dialog').text('Tak for donationen');
-                    $('#microinject-confirm').html('<img src="https://fof.local:8443/swipp.svg" style="height: 22px;"><span style="float: right;">Projektet modtager 100,-</span>');
+                    $('#microinject-confirm').html('<img src="https://raw.githubusercontent.com/Jakovitz/future-of-finance/master/src/main/webapp/swipp.svg" style="height: 22px;"><span style="float: right;">Projektet modtager 100,-</span>');
                 }, 300);
 
             });
